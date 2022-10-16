8b12431ea7d1149255322d020b84c2668684c327
Philipp K <philippkargapoloff@gmail.com>
2022-10-16 22:51:51 +0300

    добавлено задание 4

---
+++ Objective_4/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Программа вывода четных чисел от 1 до заданного числа");
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+int temp = 1;
+
+while (temp <= number)
+{
+    Console.Write(temp + " ");
+    temp++;
+}
+
+if (number < 0)
+{
+    Console.WriteLine("Невозможно отобразить числа от 1 до " + number + ". Выполняется отображение от " + number + " до 1.");
+    while (temp >= number)
+    {
+        Console.Write(number + " ");
+        number++;
+    }
+}
+
